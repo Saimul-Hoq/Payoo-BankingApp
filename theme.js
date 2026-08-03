d28fc78ad0794ac603a4bf01c4387ba93ba39e8d
Saimul-Hoq <saimulhoque2000@gmail.com>
2026-08-03 11:36:09 +0600

    Added tooltip to dark/light mode button

--- theme.js
+++ theme.js
@@ -3,6 +3,11 @@ const themeToggleBtn = document.querySelector("#theme-toggle-btn");
 
 let savedTheme = localStorage.getItem("theme") || "light";
 html.setAttribute("data-theme", savedTheme);
+if(savedTheme==="light"){
+    themeToggleBtn.dataset.tip="Dark Mode";
+}else{
+    themeToggleBtn.dataset.tip="Light Mode";
+}
 
 if(themeToggleBtn){
     themeToggleBtn.addEventListener("click", () => {
@@ -10,5 +15,10 @@ if(themeToggleBtn){
         let newTheme = currentTheme === "light" ? "dark" : "light";
         html.setAttribute("data-theme", newTheme);
         localStorage.setItem("theme", newTheme); 
+        if(newTheme==="light"){
+            themeToggleBtn.dataset.tip="Dark Mode";
+        }else{
+            themeToggleBtn.dataset.tip="Light Mode";
+        }
     });
 }
